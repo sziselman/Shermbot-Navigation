dc741dad5544b4e6a4d2553e926296ba2963886f
sziselman <sarahziselman2020@u.northwestern.edu>
2021-01-21 22:45:56 -0600

    21st commit

--- rigid2d/rigid2d.cpp
+++ rigid2d/rigid2d.cpp
@@ -8,83 +8,54 @@ using namespace rigid2d;
 
 Transform2D::Transform2D()
 {
-    T[0][0] = 1.0;
-    T[0][1] = 0.0;
-    T[0][2] = 0.0;
-    T[1][0] = 0.0;
-    T[1][1] = 1.0;
-    T[1][2] = 0.0;
-    T[2][0] = 0.0;
-    T[2][1] = 0.0;
-    T[2][2] = 1.0;
+    double T[3][3] = {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}};
 }
 
 Transform2D::Transform2D(const Vector2D & trans)
 {
-    T[0][0] = 1.0;
-    T[0][1] = 0.0;
-    T[0][2] = trans.x;
-    T[1][0] = 0.0;
-    T[1][1] = 1.0;
-    T[1][2] = trans.y;
-    T[2][0] = 0.0;
-    T[2][1] = 0.0;
-    T[2][2] = 1.0;
+    double T[3][3] = {{1.0, 0.0, trans.x}, {0.0, 1.0, trans.y}, {0.0, 0.0, 1.0}};
 }
 
 Transform2D::Transform2D(double radians)
 {
-    T[0][0] = cos(radians);
-    T[0][1] = sin(radians);
-    T[0][2] = 0.0;
-    T[1][0] = -sin(radians);
-    T[1][1] = cos(radians);
-    T[1][2] = 0.0;
-    T[2][0] = 0.0;
-    T[2][1] = 0.0;
-    T[2][2] = 1.0;
+    double T[3][3] = {{cos(radians), sin(radians), 0.0}, {-sin(radians), cos(radians), 0.0}, {0.0, 0.0, 1.0}};
 }
 
 Transform2D::Transform2D(const Vector2D & trans, double radians)
 {
-    T[0][0] = cos(radians);
-    T[0][1] = sin(radians);
-    T[0][2] = trans.x;
-    T[1][0] = -sin(radians);
-    T[1][1] = cos(radians);
-    T[1][2] = trans.y;
-    T[2][0] = 0.0;
-    T[2][1] = 0.0;
-    T[2][2] = 1.0;
+    double T[3][3] = {{cos(radians), sin(radians), trans.x}, {-sin(radians), cos(radians), trans.y}, {0.0, 0.0, 1.0}};
 }
 
 Vector2D Transform2D::operator()(Vector2D v) const
 {
-    Transform2D tMat;
-    cout << tMat.T;
     Vector2D vNew;
+
+    // Tr = tMat.T;
+    vNew.x = (T[0][0] * v.x) + (T[0][1] * v.y) + T[0][2];
+    vNew.y = (T[1][0] * v.x) + (T[1][1] * v.y) + T[1][2];
     return vNew;
+}
+
+Transform2D Transform2D::inv() const
+{
+    Transform2D Tinv;
+
+    double TinvMat[3][3] = {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}};
+    Tinv.T = TinvMat;
+    return Tinv;
 }
 
 int main()
 {
     Vector2D v1, v2;
     double rad = 2.0;
-
     cin >> v1;
     cout << v1;
 
-    cout << v2;
-
-    return 0;
+    Transform2D transformMat;
+    // v2 = Transform2D;
 }
 
-
-
-// Transform2D Transform2D::inv() const
-// {
-//     // invert the transformation
-// }
 
 // Transform2D & Transform2D::operator*=(const Transform2D & rhs)
 // {
