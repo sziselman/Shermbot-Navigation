8856de29f1f7d79ac26e662c3b1c805e1ad61f1d
sziselman <sarahziselman2020@u.northwestern.edu>
2021-01-24 15:02:46 -0600

    26th commit

---
+++ trect/turtle_rect.cpp
@@ -0,0 +1,17 @@
+#include <ros/ros.h>
+#include <iostream>
+
+
+void move(double speed, double distance, bool isForward);
+
+int main(int argc, char* argv[])
+{
+    ros::init(argc, argv, "turtle_rect");
+
+    ros::NodeHandle nh;
+}
+
+void move(double speed, double distance, bool isForward); 
+{
+
+}
