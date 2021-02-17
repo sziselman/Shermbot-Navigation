ddf70985bc27a9d04e4ad8da9010461488a5eaf8
sziselman <64819430+sziselman@users.noreply.github.com>
2021-02-17 09:08:01 -0600

    Delete rigid2d/build directory

--- rigid2d/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sarah/Documents/ME_495/ws/src/repository/rigid2d
-# Build directory: /home/sarah/Documents/ME_495/ws/src/repository/rigid2d/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test_of_Rigid2d "/home/sarah/Documents/ME_495/ws/src/repository/rigid2d/build/rigid_test")
-set_tests_properties(Test_of_Rigid2d PROPERTIES  _BACKTRACE_TRIPLES "/home/sarah/Documents/ME_495/ws/src/repository/rigid2d/CMakeLists.txt;16;add_test;/home/sarah/Documents/ME_495/ws/src/repository/rigid2d/CMakeLists.txt;0;")
