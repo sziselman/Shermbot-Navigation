8df1bc0c5340fa9b7856fb6e685420c31772efcb
sziselman <sarahziselman2020@u.northwestern.edu>
2021-02-16 18:07:18 -0600

    starting Task F.007

--- nuturtle_robot/src/follow_circle.cpp
+++ nuturtle_robot/src/follow_circle.cpp
@@ -8,13 +8,24 @@
 /// SERVICES:
 
 #include <ros/ros.h>
+#include <nuturtle_robot/control.h>
+
 #include <rigid2d/rigid2d.hpp>
 #include <rigid2d/diff_drive.hpp>
 
+#include <string>
+
+/***************
+ * Declare global variables
+ * ************/
 static double radius;
 static double speed;
+static int ccw = true;
+static ros::ServiceServer control_service;
+static ros::ServiceClient control_client;
 
 static ros::Publisher wheelCommand_pub;
+
 int main(int argc, char* argv[])
 {
     /****************
@@ -30,6 +41,13 @@ int main(int argc, char* argv[])
     n.getParam("radius", radius);
     n.getParam("speed", speed);
 
+    /****************
+     * Define publisher, subscriber, services and clients
+     * *************/
+    wheelCommand_pub = n.advertise<nuturtlebot::WheelCommands>("/wheel_cmd", frequency);
+    control_service = n.advertiseService("/control", control);
+    control_client = n.serviceClient<nuturtle_robot::control>("/control");
+    
     ros::Rate loop_rate(frequency);
     while (ros::ok())
     {
@@ -37,4 +55,23 @@ int main(int argc, char* argv[])
         loop_rate.sleep();
     }
     return 0;
+}
+
+/// \brief control function for /control service
+/// The service causes the robot to move counter-clockwise, clockwise or stop
+/// \param req : The service request
+/// \param res : The service response
+/// \return true
+bool control(nuturtle_robot::control::Request &req, nuturtle_robot::control::Response &res)
+{
+    if (req.direction == "ccw")
+    {
+        ccw = true;
+    } else if (req.direction == 'cw')
+    {
+        ccw = false;
+    } else if (req.direction == 'stop')
+    {
+        // stop the robot!!!!!
+    }
 }
