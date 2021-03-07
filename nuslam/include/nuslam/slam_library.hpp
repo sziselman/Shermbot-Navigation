7b9f4556802f4171f95144841ac8007239e6ad6b
sziselman <sarahziselman2020@u.northwestern.edu>
2021-03-07 09:48:19 -0600

    slam_library

--- nuslam/include/nuslam/slam_library.hpp
+++ nuslam/include/nuslam/slam_library.hpp
@@ -7,9 +7,53 @@
 #include"rigid2d/rigid2d.hpp"
 #include"rigid2d/diff_drive.hpp"
 
-namespace slam_lib
+namespace slam_library
 {
-    
+    using namespace arma;
+    using namespace rigid2d;
+
+    /// \brief a function that generates multivariate gaussian noise
+    /// \param mean 1xn column vector
+    /// \param var nxn matrix
+    colvec MultiGauss(const colvec mean, const mat var);
+
+
+    /// \brief a class that contains functions when utilizing Extended Kalman Filter
+    /// At each time step t, the EKF takes odometry (u) and sensor measurements (z)
+    /// to generate estimate of full state vector (zeta)
+    class ExtendedKalman
+    {
+        private:
+            // the number of landmarks
+            int n;
+            int size = 3+(2*n);
+            double x;
+            double y;
+            double th;
+
+            colvec stateVec;
+            colvec processMean;
+            mat processNoise;
+
+        public:
+            ExtendedKalman(double xx, double yy, double theta, int num);
+
+            /// \brief generates an estimate of the fulls tate vector z_hat
+            /// \return estimated state vector z_t = g(z_{t-1}, u_t, w_t)
+            colvec getEstimate(const Twist2D & tw);
+
+            /// \brief gets the matrix A_k using the state k-1
+            /// \return a (3+2n)x(3+2n) matrix
+            mat getA(const Twist2D & tw);
+
+            /// \brief gets the matrix H_k using the state k-1
+            /// \return a 2x(3+2n) matrix
+            mat getW();
+
+            /// \brief gets the matrix V_k using the state k-1
+            /// \return the multivariate Gaussian noise V_k~N(0,R)
+            mat getV();
+    };
 }
 
 #endif
--- nuslam/src/slam_library.cpp
+++ nuslam/src/slam_library.cpp
@@ -2,8 +2,98 @@
 #include "rigid2d/rigid2d.hpp"
 #include "rigid2d/diff_drive.hpp"
 #include <armadillo>
+#include <cmath>
+#include <random>
+
+std::mt19937 & get_random()
+{
+     // static variables inside a function are created once and persist for the remainder of the program
+     static std::random_device rd{}; 
+     static std::mt19937 mt{rd()};
+     // we return a reference to the pseudo-random number genrator object. This is always the
+     // same object every time get_random is called
+     return mt;
+ }
 
 namespace slam_library
 {
-    
+    using namespace arma;
+    using namespace rigid2d;
+
+    colvec MultiGauss(const colvec mean, const mat var)
+    {
+        mat L = chol(var);
+
+        colvec u(size(mean));
+        std::normal_distribution<> d(0, 1);
+        for (int i = 0; i < mean.n_elem; ++i)
+        {
+            u(i) = d(get_random());
+        }
+
+        colvec v = mean + L*u;
+        return v;
+    }
+
+    ExtendedKalman::ExtendedKalman(double xx, double yy, double theta, int num)
+    {
+        n = num;
+        x = xx;
+        y = yy;
+        th = theta;
+        stateVec(0) = theta;
+        stateVec(1) = xx;
+        stateVec(2) = yy;
+
+        processMean(0) = 0;
+        processMean(1) = 0;
+        processMean(2) = 0;
+    }
+
+    colvec ExtendedKalman::getEstimate(const Twist2D & tw)
+    {
+        colvec estimate;
+
+        colvec vec1(size, fill::zeros);
+
+        if (tw.dth == 0)
+        {
+            vec1(0) = 0;
+            vec1(1) = tw.dx * cos(th);
+            vec1(2) = tw.dx * sin(th);
+        } else
+        {
+            vec1(0) = tw.dth;
+            vec1(1) = -(tw.dx/tw.dth)*sin(th) + (tw.dx/tw.dth)*sin(th+tw.dth);
+            vec1(2) = (tw.dx/tw.dth)*cos(th) - (tw.dx/tw.dth)*cos(th+tw.dth);
+        }
+
+        colvec vec2(size, fill::zeros);
+        colvec wt = MultiGauss(processMean, processNoise);
+
+        vec2(0) = wt(0);
+        vec2(1) = wt(1);
+        vec2(2) = wt(2);
+
+        estimate = stateVec + vec1 + vec2;
+        return estimate;
+    }
+
+    mat ExtendedKalman::getA(const Twist2D & tw)
+    {
+        mat B(size, size, fill::zeros);
+
+        mat I(size, size, fill::eye);
+
+        if (tw.dth == 0)
+        {
+            B(0, 1) = -tw.dx * sin(th);
+            B(0, 2) = tw.dx * cos(th);
+        } else {
+            B(0, 1) = -(tw.dx/tw.dth)*cos(th) + (tw.dx/tw.dth)*cos(th + tw.dth);
+            B(0, 2) = -(tw.dx/tw.dth)*sin(th) + (tw.dx/tw.dth)*sin(th + tw.dth);
+        }
+        mat A = I + B;
+        return A;
+    }
 }
