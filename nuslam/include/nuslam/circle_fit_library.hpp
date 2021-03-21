0318c841603327cc3ff72c369b128af2aac57c96
sziselman <sarahziselman2020@u.northwestern.edu>
2021-03-20 20:33:20 -0500

    create circle fit lib

---
+++ nuslam/include/nuslam/circle_fit_library.hpp
@@ -0,0 +1,11 @@
+#ifndef CIRCLE_FIT_LIBRARY_INCLUDE_GUARD_HPP
+#define CIRCLE_FIT_LIBRARY_INCLUDE_GUARD_HPP
+/// \file
+/// \brief Library for circle fit algorithm
+
+namespace circle_fit
+{
+
+}
+
+#endif
---
+++ nuslam/src/circle_fit_library.cpp
@@ -0,0 +1,9 @@
+/// \file circle_fit_library.cpp
+/// \brief a library that implements circle fit algorithm
+
+#include "nuslam/circle_fit_library.hpp"
+
+namespace circle_fit
+{
+    
+}
